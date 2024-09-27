f6830d46a61e6727b7f409a3dd3f2e06fb406bb9
Gerardo Lanza <glanza007@gmail.com>
2024-09-27 13:56:41 -0600

    Create project

---
+++ Feeds/Feeds/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Feeds.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
